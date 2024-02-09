8e2c3495b0b9cb368789ed8d58b228fec1cf113c
totowen <136248471@qq.com>
2024-02-09 23:02:18 +0800

    统计N以内的素数

---
+++ src/main/java/org/example/study/CountPrimes.java
@@ -0,0 +1,42 @@
+package org.example.study;
+
+/**
+ * 统计N以内的素数
+ * 素数：只能被1和自身整除的数，0、1除外
+ * 解题：从2开始遍历，范围为小于n的整数x，x余数为0的情况等于2的数
+ */
+public class CountPrimes {
+
+
+    public static void main(String[] args) {
+        CountPrimes countPrimes = new CountPrimes();
+        System.out.println(countPrimes.countPrimes(20));
+    }
+
+    public int countPrimes(int n) {
+
+        if (n == 0 || n == 1) {
+            return 0;
+        }
+
+        int count = 0;
+        for (int i = 2; i <=n; i++) {
+            int temp = 0;
+            for (int j = 1; j <= n; j++) {
+                if (i % j == 0) {
+                    temp++;
+                }
+                if(temp>2){
+                    break;
+                }
+            }
+            if(temp==2){
+                System.out.println(i);
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+}
